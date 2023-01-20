7f78837b47468957296d8ec66e4a55ba66729756
William Voigt <wvoigt@Williams-MacBook-Air.local>
2023-01-20 14:27:21 -0800

    updated fiels in views and controllers

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,9 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    res.render('homepage');
+    res.render('homepage', {
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -12,7 +14,29 @@ router.get('/', async (req, res) => {
 
 router.get('/login', async (req, res) => {
   try {
-    res.render('login');
+    res.render('login', {
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    res.render('dashboard', {
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/results', withAuth, async (req, res) => {
+  try {
+    res.render('results', {
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
